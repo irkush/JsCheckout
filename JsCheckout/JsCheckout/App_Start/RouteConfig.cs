2cca1ec01583e4291cbe262ac61df3eb97c6226c
Daniel Ekroth <daniel.ekroth@sveaekonomi.se>
2013-12-12 09:46:22 +0100

    Basic bootstrap page

--- JsCheckout/JsCheckout/App_Start/RouteConfig.cs
+++ JsCheckout/JsCheckout/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace JsCheckout
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Basic", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
