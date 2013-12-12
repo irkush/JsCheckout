bfd00932d322494991d28634189358cc0b3d563c
Sinsabre <simon.wiippola@sveaekonomi.se>
2013-12-12 15:58:18 +0100

    I give up...

---
+++ JsCheckout/JsCheckout/Scripts/models/ember-checkout.js
@@ -0,0 +1,34 @@
+﻿Checkouts.Checkout = DS.Model.extend({
+    beskrivning: DS.attr('string'),
+    antal: DS.attr('float'),
+    kostnad: DS.attr('float'),
+    moms: DS.attr('integer'),
+    totalt: DS.attr('float')
+});
+
+Checkouts.Checkout.FIXTURES = [
+ {
+     id: 1,
+     beskrivning: 'Ember rad 1',
+     antal: 10.0,
+     kostnad: 100,
+     moms: 25,
+     totalt: 10 * 100
+ },
+ {
+     id: 2,
+     beskrivning: 'Ember rad 2',
+     antal: 20.0,
+     kostnad: 200,
+     moms: 25,
+     totalt: 20 * 200
+ },
+ {
+     id: 3,
+     beskrivning: 'Ember rad 3',
+     antal: 30.0,
+     kostnad: 300,
+     moms: 25,
+     totalt: 30 * 300
+ }
+];
