568a9d3251f2ff246b5a179530c6d6c80790d2b1
unknown <robi-bro@pc614.svea_dom>
2013-12-13 10:40:02 +0100

    Added knockout...

---
+++ JsCheckout/JsCheckout/Scripts/KnockoutTest.js
@@ -0,0 +1,34 @@
+﻿function Item(description, quantity, amount, vat) {
+    this.description = ko.observable(description);
+    this.quantity = ko.observable(quantity);
+    this.amount = ko.observable(amount);
+    this.vat = ko.observable(vat);
+    this.total = ko.dependentObservable(function () {
+        return (this.amount() * ((vat / 100) + 1) * this.quantity()).toFixed(2);
+    }, this);
+}
+
+function AppViewModel() {
+    var self = this;
+
+    self.items = ko.observableArray([new Item("Example", "1", "2", "3")]);
+    self.addItem = function() {
+        self.items.push(new Item("New", "", "", ""));
+    };
+    self.removeItem = function(item) {
+        self.items.remove(item);
+    };
+    
+    self.orderTotal = ko.dependentObservable(function () {
+        var total = 0;
+        ko.utils.arrayForEach(this.items(), function (item) {
+            var value = parseFloat(item.total());
+            if (!isNaN(value)) {
+                total += value;
+            }
+        });
+        return total.toFixed(2);
+    }, self);
+                                
+}
+ko.applyBindings(new AppViewModel());
